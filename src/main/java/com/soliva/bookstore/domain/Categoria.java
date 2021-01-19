b9892bd5fd60eafff6708c5f61bbae151daabd52
Michel Soliva <micheldev2020@hotmail.com>
2021-01-18 23:04:36 -0300

    Classes de Dominio Iniciadas

---
+++ src/main/java/com/soliva/bookstore/domain/Categoria.java
@@ -0,0 +1,86 @@
+package com.soliva.bookstore.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	private String name;
+	
+	private String descricao;
+	
+	private List<Livro> livros = new ArrayList<>();
+
+	
+	public Categoria() {
+		
+	}
+	
+	public Categoria(Integer id, String name, String descricao) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
