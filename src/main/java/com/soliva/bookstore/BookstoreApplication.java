fab9b8bc3ce3356aa74516f466e9e3a7c59e10e5
Michel Soliva <micheldev2020@hotmail.com>
2021-01-18 23:21:30 -0300

    Instancia automática da base de dados

--- src/main/java/com/soliva/bookstore/BookstoreApplication.java
+++ src/main/java/com/soliva/bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package com.soliva.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.soliva.bookstore.domain.Categoria;
+import com.soliva.bookstore.domain.Livro;
+import com.soliva.bookstore.repositories.CategoriaRepository;
+import com.soliva.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem Ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
 }
