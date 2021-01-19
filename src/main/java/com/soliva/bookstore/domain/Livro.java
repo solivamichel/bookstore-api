bd94a0f1bf8beb63bf6dd373f3db839a28beea47
Michel Soliva <micheldev2020@hotmail.com>
2021-01-18 23:09:07 -0300

    Finalizando classes de dominio

--- src/main/java/com/soliva/bookstore/domain/Livro.java
+++ src/main/java/com/soliva/bookstore/domain/Livro.java
@@ -2,9 +2,19 @@ package com.soliva.bookstore.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Livro implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String titulo;
@@ -13,6 +23,8 @@ public class Livro implements Serializable {
 	
 	private String texto;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 	
 	public Livro() {
