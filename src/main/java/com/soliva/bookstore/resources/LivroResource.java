b55ed2e24e9369f6f1b77f842a7fe603486af596
Michel Soliva <micheldev2020@hotmail.com>
2021-01-25 19:34:34 -0300

    Listando todos os livros por categoria

--- src/main/java/com/soliva/bookstore/resources/LivroResource.java
+++ src/main/java/com/soliva/bookstore/resources/LivroResource.java
@@ -1,13 +1,18 @@
 package com.soliva.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soliva.bookstore.domain.Livro;
+import com.soliva.bookstore.dtos.LivroDTO;
 import com.soliva.bookstore.sevice.LivroService;
 
 @RestController
@@ -21,5 +26,12 @@ public class LivroResource {
 	public ResponseEntity<Livro> findById( @PathVariable Integer id ) {
 		Livro obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll( @RequestParam(value = "categoria", defaultValue = "0") Integer id_cat ) {
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/soliva/bookstore/sevice/LivroService.java
+++ src/main/java/com/soliva/bookstore/sevice/LivroService.java
@@ -1,5 +1,6 @@
 package com.soliva.bookstore.sevice;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,16 @@ public class LivroService {
 	@Autowired
 	private LivroRepository livroRepository;
 
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = livroRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id" + id + ", Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return livroRepository.findAllByCategoria(id_cat);
 	}
 }
