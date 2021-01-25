4b58294e4b8538bfc0eed11fa8901482d1224113
Michel Soliva <micheldev2020@hotmail.com>
2021-01-25 13:38:48 -0300

    Post para categoria

--- src/main/java/com/soliva/bookstore/sevice/CategoriaService.java
+++ src/main/java/com/soliva/bookstore/sevice/CategoriaService.java
@@ -24,4 +24,9 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
 	}
+	
+	public Categoria create( Categoria obj ) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
 }
