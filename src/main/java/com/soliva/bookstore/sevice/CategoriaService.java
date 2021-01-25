7ca3fad04e6dee10385df53eb37a1fdae03d12f9
Michel Soliva <micheldev2020@hotmail.com>
2021-01-25 18:37:33 -0300

    Exceção DataIntegrityViolationException implementada

--- src/main/java/com/soliva/bookstore/sevice/CategoriaService.java
+++ src/main/java/com/soliva/bookstore/sevice/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.soliva.bookstore.domain.Categoria;
@@ -40,6 +41,11 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		findById(id);
-		categoriaRepository.deleteById(id);
+		
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.soliva.bookstore.service.exceptions.DataIntegrityViolationException("Objeto não pode ser deletada! possui livros associados");
+		}
 	}
 }
